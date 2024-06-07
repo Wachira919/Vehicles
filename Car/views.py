9e79adc8806fb0fb4bf43f209925cba57d086a51
Wachira919 <maxwellwachira100@gmail.com>
2024-06-07 14:58:39 +0300

    first django project

--- Car/views.py
+++ Car/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# def index(request):
+#     return HttpResponse("")
+
+# def about(request):
+#     return HttpResponse("<h1> About Page</h1>")
+
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
